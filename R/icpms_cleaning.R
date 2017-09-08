b2d3bdd701aeee5511e6a03d45fb22cc3c0e480a
Dewey Dunnington <dewey@fishandwhistle.net>
2017-09-08 12:32:11 -0300

    update to pass cmd check

--- R/icpms_cleaning.R
+++ R/icpms_cleaning.R
@@ -1,52 +1,115 @@
-library(tidyverse)
-library(readxl)
-library(stringr)
-rm(list = ls())
 
-# The input to the function is the path where the excel files (.xls) are stored
-icpms <- function(path){
-  #Initialize data frame
-  final <- as_tibble()
-  files <- list.files(path, pattern='*.xlsx') 
-  
-  for(i in files){
-    # Read in file
-    data_file <- read_excel(paste(path, i, sep = "/"), skip = 4) %>% select(1:(ncol(.)-1))
-    
-    # Change column names to element plus unit
-    names(data_file) <- data_file %>% names(.) %>% str_extract(., "([:alpha:]+)") %>%
-      paste(., data_file[1,], sep = "_") 
-    
-    # Create vector of sample ids
-    sample.ids <- distinct(data_file[-1,3]) %>% filter(!is.na(.), .!=0)
-    names(sample.ids) <- "sample_id"
-    
-    # Vectors of means and sd's
-    m <- filter(data_file[,-c(1:3)], data_file[,1]=="x") %>% mutate(type = "mean")
-    s <- filter(data_file[,-c(1:3)], data_file[,1]=="s") %>% mutate(type = "sd")
-    
-    # Bind values with ids
-    means <- bind_cols(sample.ids, m)
-    sds <- bind_cols(sample.ids, s)
-    
-    # Put everything together and clean it up
-    temp <- rbind(means, sds) %>% 
-      gather(-sample_id, -type, key = "param", value = "value") %>%
-      mutate(numeric_value = suppressWarnings(as.numeric(value))) %>% 
-      #all NAs are from QC sd's
-      filter(!is.na(numeric_value)) %>%
-      separate(param, c("param", "unit"), sep = "_") %>%
-      mutate(param = str_to_lower(param)) %>%
-      select(sample_id, param, type, value = numeric_value, unit) 
-    
-    final <- bind_rows(final, temp)
+#' Read ICP-MS Data Files
+#'
+#' @param path The path where the files are kept
+#' @param df The output of \code{read_icpms} or a filtered result
+#' @param skip The number of lines to skip at the beginning of the file
+#'   (blank lines are automatically dropped).
+#' @param na Values that should be considered NA
+#'
+#' @return \code{read_icpms} returns a
+#'   data frame with columns sample_id, datetime, run, and others in
+#'   the form "27Al_ppb" (isotope, element, unit). \code{tidy_icpms} returns
+#'   a parameter-long, summarised result.
+#' @export
+#'
+#' @importFrom dplyr select filter mutate mutate_at vars starts_with everything
+#' @importFrom dplyr summarise group_by ungroup if_else
+#' @importFrom magrittr %>%
+#'
+#' @examples
+#' # file
+#' read_icpms(system.file("test_icpms/test_icpms.xlsx", package = "dalcwrs"))
+#' # directory
+#' read_icpms(system.file("test_icpms", package = "dalcwrs"))
+#'
+#' # use tidy to get parameter-long, summarised output
+#' raw <- read_icpms(system.file("test_icpms/test_icpms.xlsx", package = "dalcwrs"))
+#' tidy_icpms(raw)
+#'
+read_icpms <- function(path, skip = 0, na = c("NA", "", "n/a")) {
+  # if is a directory, read all excel files in the directory and rbind
+  if(dir.exists(path)) {
+    # list files in directory
+    files <- list.files(path, pattern = '*.xlsx', full.names = TRUE)
+
+    # return result of read_icpms_single rbinded together
+    return(purrr::map_dfr(files, read_icpms, skip = skip, na = na))
   }
-  return(final)
+
+  # read in file with all columns as character
+  data_file <- readxl::read_excel(path, col_types = "text", na = na) %>%
+    select(-starts_with("X_"))
+
+  # check headers to make sure all required headers are present
+  required_cols <- c("Run", "Time")
+  missing_cols <- required_cols[!(required_cols %in% names(data_file))]
+  if(length(missing_cols) > 0) stop(path, " is missing required columns: ",
+                                    paste(missing_cols, collapse = ", "))
+
+  # Change column names to element plus unit
+  data_file_names <- names(data_file)
+  data_file_names[c(-1, -2)] <- paste(data_file_names[c(-1, -2)],
+                                      data_file[1, c(-1, -2)],
+                                      sep = "_")
+  names(data_file) <- data_file_names
+
+  # make sample ID column
+  # make things from the sample_id column that aren't sample ids NA
+  data_file$sample_id <- data_file[[3]] %>%
+    if_else(stringr::str_detect(., " [AP]M$"), ., NA_character_)
+
+  # final format of data_file
+  data_file %>%
+    # fill sample ID down to apply to all replicates
+    tidyr::fill(sample_id, .direction = "down") %>%
+    # filter where run is not NA
+    filter(!is.na(Run)) %>%
+    # extract sample id, datetime from sample ID column
+    tidyr::extract(sample_id, into = c("sample_id", "datetime"),
+                   paste("^(.*?)\\s+([0-9]{1,2}/[0-9]{1,2}/[0-9]{4}",
+                         "[0-9]{1,2}:[0-9]{1,2}:[0-9]{1,2}\\s[AP]M$)",
+                         sep = "\\s+")) %>%
+    # parse date_run
+    mutate(datetime = lubridate::mdy_hms(datetime)) %>%
+    # select final order
+    select(sample_id, datetime, run = Run, everything()) %>%
+    select(-Time) %>%
+    # make value columns numeric
+    mutate_at(vars(-sample_id, -datetime, -run), as.numeric)
 }
 
-##### Check 
-path <- "C:/Users/Owner/Documents/Lab Data/ICPMS/raw"
+#' @rdname read_icpms
+#' @export
+tidy_icpms <- function(df) {
+  # check input
+  if(!is.data.frame(df)) stop("df is not a data frame")
 
-X <- icpms(path = path)
+  # check required columns
+  required_cols <- c("sample_id", "datetime", "run")
+  missing_cols <- required_cols[!(required_cols %in% names(df))]
+  if(length(missing_cols) > 0) stop("df is missing required columns: ",
+                                    paste(missing_cols, collapse = ", "))
 
-
+  # gather, summarise data columns
+  df %>%
+    # filter only raw data, not summaries
+    filter(stringr::str_detect(run, "^[0-9]+$")) %>%
+    select(-run) %>%
+    # gather data values
+    tidyr::gather(-sample_id, -datetime, key = "param", value = "value") %>%
+    # summarise data values to value, sd, n
+    group_by(sample_id, datetime, param) %>%
+    summarise(sd = stats::sd(value, na.rm = TRUE),
+              n = n(),
+              n_na = sum(is.na(value)),
+              value = mean(value, na.rm = TRUE),
+              rsd = sd / value * 100) %>%
+    ungroup() %>%
+    # extract element, isotope, unit from param
+    tidyr::extract(param, into = c("isotope", "element", "unit"),
+                   "^([0-9.]+)([A-Za-z]+)_(.*?)$") %>%
+    mutate(isotope = as.numeric(isotope)) %>%
+    # final column selection
+    select(sample_id, datetime, isotope, element, value, sd, unit, n, n_na, rsd)
+}
--- R/reading_toc.R
+++ R/reading_toc.R
@@ -1,41 +1,38 @@
-rm(list=ls())
-library(readxl)
-library(tidyr)
-library(dplyr)
-library(stringr)
-
-path <- "C:/Users/Owner/Documents/Lab Data/TOC-DOC"
-
-# Function to create master file of DOC/TOC/TN data
- toc_readr <- function(path){
-   
-   final <- as_tibble()
-   files <- list.files(path, pattern='*.xlsx|*.xls') 
-   
-   data_list <- lapply(files, function(i) read_excel(paste(path,"/", i, sep = ''),
-                col_names = FALSE))
-   
-   data_tibble <- as_tibble()
-   for(i in files){
-     # Read in file
-     data <- read_excel(paste(path, i, sep = "/"), col_names = FALSE) %>% select(1:2)
-     data_tibble <- bind_rows(data_list, data)
-   }
-   final <- data_tibble %>% 
-     select(x1 = X__1, x2 = X__2) %>% 
-     extract(x2, "toc_doc", "NPOC:([0-9.]+)", remove = FALSE) %>%  
-     extract(x2, "tn", "TN:([0-9.]+)") %>%  
-     extract(x1, c("type"), "([TD]OC)", remove=FALSE) %>% 
-     mutate(x1 = str_replace(x1, type, "")) %>% 
-     mutate(x1 = str_replace(x1, "/TN", "")) %>% 
-     filter(!is.na(type)) %>% 
-     gather(-x1, -type, key = "param", value = "value") %>%
-     filter(!is.na(value)) 
- }   
-
-# specify folder containing toc/doc data
-X <- toc_readr(path)
-
-
-
-
+# rm(list=ls())
+# library(readxl)
+# library(tidyr)
+# library(dplyr)
+# library(stringr)
+#
+# path <- "C:/Users/Owner/Documents/Lab Data/TOC-DOC"
+#
+# # Function to create master file of DOC/TOC/TN data
+#  toc_readr <- function(path){
+#
+#    final <- as_tibble()
+#    files <- list.files(path, pattern='*.xlsx|*.xls')
+#
+#    data_list <- lapply(files, function(i) read_excel(paste(path,"/", i, sep = ''),
+#                 col_names = FALSE))
+#
+#    data_tibble <- as_tibble()
+#    for(i in files){
+#      # Read in file
+#      data <- read_excel(paste(path, i, sep = "/"), col_names = FALSE) %>% select(1:2)
+#      data_tibble <- bind_rows(data_list, data)
+#    }
+#    final <- data_tibble %>%
+#      select(x1 = X__1, x2 = X__2) %>%
+#      extract(x2, "toc_doc", "NPOC:([0-9.]+)", remove = FALSE) %>%
+#      extract(x2, "tn", "TN:([0-9.]+)") %>%
+#      extract(x1, c("type"), "([TD]OC)", remove=FALSE) %>%
+#      mutate(x1 = str_replace(x1, type, "")) %>%
+#      mutate(x1 = str_replace(x1, "/TN", "")) %>%
+#      filter(!is.na(type)) %>%
+#      gather(-x1, -type, key = "param", value = "value") %>%
+#      filter(!is.na(value))
+#  }
+#
+# # specify folder containing toc/doc data
+# X <- toc_readr(path)
+#
